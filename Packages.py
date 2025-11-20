c917bfa8f33d13d44fc4c8baee3541d55094eb20
Sachinkannan127 <Sachinkannan0515@gmail.com>
2025-11-20 12:07:31 +0530

    Add imports for data analysis and modeling

---
+++ Packages.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn.dataset import fetch_california_housing
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
